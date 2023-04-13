31bbb33b6aa4e2274d8a77a621ab11cb721d6a52
Arshak Gasparyan <arshakgasparyan1@gmail.com>
2023-04-14 02:50:44 +0400

    some changes

---
+++ existServices/vpc.sh
@@ -0,0 +1,37 @@
+#!bin/bash
+
+function checkVpc() {
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
+    id=$(echo $1 | grep 'vpc-')
+    vpcId=$1
+    if [ $id ];
+    then
+        vpc=$( aws ec2 describe-vpcs --vpc-ids $id 2>&1)
+        if [[ $? -ne 0 ]];
+        then
+            read -p "This VPC does't exist can I take default VPC? [Y/n]: " flag
+            ans=$(echo $flag | grep -E '\by|Y|yes|YES|Yes\b')
+            if [[ $ans ]];
+            then
+                vpc=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true")
+                vpcId=$(echo -e "$vpc" |  /usr/bin/jq '.Vpcs[0].VpcId' | tr -d '"')
+            else
+                source "$SCRIPT_DIR/cleanup.sh"
+                cleanUp
+                return
+            fi
+        fi
+    else
+        read -p "It is unavailable ID can I take default VPC? [Y/n]: " flag
+        ans=$(echo $flag | grep -E '\by|Y|yes|YES|Yes\b')
+        if [[ $ans ]];
+        then
+            vpc=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true")
+            vpcId=$(echo -e "$vpc" |  /usr/bin/jq '.Vpcs[0].VpcId' | tr -d '"')
+        else
+            source "$SCRIPT_DIR/cleanup.sh"
+            cleanUp
+            return
+        fi
+    fi
+}
